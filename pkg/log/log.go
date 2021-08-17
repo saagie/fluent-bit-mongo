7d3b6da56fdbaa01ac8ee1129eadff05e7e3d716
Pierre PÉRONNET <pierre.peronnet@gmail.com>
2021-08-17 15:21:00 +0200

    feat(logger) New logger package

---
+++ pkg/log/log.go
@@ -0,0 +1,87 @@
+package log
+
+import (
+	"fmt"
+	"os"
+	"sync"
+
+	"github.com/go-logr/logr"
+	"github.com/go-logr/zapr"
+	"github.com/saagie/fluent-bit-mongo/pkg/log/encoder"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
+
+// https://github.com/fluent/fluent-bit/blob/03f3339594bdb472315823db78ca209b7ba319fb/src/flb_config.c#L433-L441
+const FluentBitLogLevelEnvironment = "FLB_LOG_LEVEL"
+
+type Logger interface {
+	Debug(string, map[string]interface{})
+	Info(string, map[string]interface{})
+	Error(string, map[string]interface{})
+}
+
+type logger struct {
+	log logr.Logger
+}
+
+var registerEncoderOnce sync.Once
+
+func New(t PluginType, name string) (Logger, error) {
+	zc := zap.NewProductionConfig()
+
+	level := zapcore.InfoLevel
+	levelStr := os.Getenv(FluentBitLogLevelEnvironment)
+	if levelStr == "" {
+		level.UnmarshalText([]byte(levelStr))
+	}
+	zc.Level = zap.NewAtomicLevelAt(level)
+	zc.DisableStacktrace = true
+
+	registerEncoderOnce.Do(func() {
+		zap.RegisterEncoder("fluent-bit", encoder.New)
+	})
+	zc.Encoding = "fluent-bit"
+
+	z, err := zc.Build()
+	if err != nil {
+		return nil, fmt.Errorf("build log config: %w", err)
+	}
+
+	log := zapr.NewLogger(z)
+
+	return &logger{
+		log: log.WithName(fmt.Sprintf("%s:%s", t.String(), name)),
+	}, nil
+}
+
+func (l *logger) Debug(message string, args map[string]interface{}) {
+	l.log.V(1).Info(message, l.argsToMeta(args)...)
+}
+
+func (l *logger) Info(message string, args map[string]interface{}) {
+	l.log.Info(message, l.argsToMeta(args)...)
+}
+
+func (l *logger) Error(message string, args map[string]interface{}) {
+	var err error
+	if err2, ok := args["error"]; ok {
+		err = err2.(error)
+	}
+
+	l.log.Error(err, message, l.argsToMeta(args)...)
+}
+
+func (l *logger) argsToMeta(args map[string]interface{}) []interface{} {
+	meta := make([]interface{}, len(args)*2)
+
+	i := 0
+	for k, v := range args {
+		meta[i] = k
+		i += 1
+		meta[i] = v
+		i += 1
+	}
+
+	return meta
+}
